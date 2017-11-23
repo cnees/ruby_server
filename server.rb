76da108385cb1e54d41734465e3d7f1caf99b5eb
Clara Nees <cnees@umich.edu>
2017-11-23 14:45:33 -0800

    Add Content-Type HTTP header
    Server, Date, Content-Length, and Connection headers are already
    added automatically.
    
    Example response:
    
    HTTP/1.1 200 OK
    Content-Type: text/html; charset=utf-8
    Server: WEBrick/1.3.1 (Ruby/2.3.0/2015-12-25)
    Date: Thu, 23 Nov 2017 22:55:30 GMT
    Content-Length: 22
    Connection: Keep-Alive
    http://localhost:8080/

--- server.rb
+++ server.rb
@@ -1,7 +1,11 @@
 require 'rack'
 
 def request_handler(env)
-  return [200, {}, [env['REQUEST_URI']]]
+  return [
+    200,
+    {'Content-Type' => 'text/html; charset=utf-8'},
+    [env['REQUEST_URI']]
+  ]
 end
 
 Rack::Handler::WEBrick.run Proc.new{|env| request_handler(env)}
