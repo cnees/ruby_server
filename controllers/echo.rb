afad4fde759b5d17dd3644ede650d71f89387a18
Clara Nees <cnees@umich.edu>
2018-01-04 17:35:23 -0500

    Format /echo content like an HTTP request

--- controllers/echo.rb
+++ controllers/echo.rb
@@ -1,7 +1,20 @@
 class Echo < AbstractController
   def get(env)
+    request_method = env['REQUEST_METHOD']
+    request_uri = env['REQUEST_URI']
+    http_version = env['HTTP_VERSION']
+
+    start_line = "#{request_method} #{request_uri} #{http_version}"
+
+    headers = env.
+      select{|key, value| key.start_with?('HTTP_') && key != 'HTTP_VERSION' }.
+      map{|key, value| "#{key.split("_")[1..-1].join('-')}: #{value}"}
+
+    request_body = "\n" + env['rack.input'].read
+
     {
-      body: env.to_s
+      headers: {'Content-Type' => 'text/plain; charset=utf-8'}.freeze,
+      body: [start_line, headers, request_body].flatten.join("\n")
     }
   end
 end
